0f065c7e0ac46e9a9dda632fc2cca499da1aa0bd
Janna Pierre <jannapierre@gmail.com>
2019-11-04 18:01:56 -0500

    Added Router to App.js and created reservation.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,32 @@ import Header from './Header';
 import Nav from './Nav';
 import Footer from './Footer';
 import Main from './Main';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+}
+from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render(){
   return (
     <div className="App">
+
+    <Router>
+        <div>
+          <nav>
+            <Link to="/">Go to Home Page</Link>{' '}
+            <Link to="/main">See Our Procedures</Link>{' '}
+            <Link to="/reservation">Make a reservation</Link>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/main" component={Main} />
+          <Route path="/reservation" component={Reservation} />
+        </div>
+      </Router>
       <Header />
       <Nav />
       <Main />
---
+++ frontend/src/Reservation.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Reservation extends Component {
+  render(){
+    return(
+      <div className="Reservation">
+      <h1>Reservation page</h1>
+      </div>
+    );
+  }
+}
+  export default Reservation;
