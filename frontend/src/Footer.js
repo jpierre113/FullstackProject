c110e7ec3ceba4c9d6362ceb26fad306c6f05ab6
Janna Pierre <jannapierre@gmail.com>
2019-11-04 17:41:37 -0500

    Created Footer.js

---
+++ frontend/src/Footer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render(){
+    return(
+      <div className="Footer">
+      <h1>Footer page</h1>
+      </div>
+    );
+  }
+}
+  export default Footer;
