5a350ee70327caebcb1248bf37fd45f750fac15e
Janna Pierre <jannapierre@gmail.com>
2019-11-04 17:40:57 -0500

    Created Header.css and Nav.js

---
+++ frontend/src/Nav.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+  render(){
+    return(
+      <div className="Nav">
+      <h1>Nav page</h1>
+      </div>
+    );
+  }
+}
+  export default Nav;
