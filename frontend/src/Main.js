8685a5276564a78d464b2dd2136641a98b44b7fd
Janna Pierre <jannapierre@gmail.com>
2019-11-04 17:42:07 -0500

    Created Main.js

---
+++ frontend/src/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+  render(){
+    return(
+      <div className="Main">
+      <h1>Main page</h1>
+      </div>
+    );
+  }
+}
+  export default Main;
