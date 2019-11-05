3ec9a43b6e253603dc9ea690095556c4a8d544fc
Janna Pierre <jannapierre@gmail.com>
2019-11-05 10:52:08 -0500

    Added Rest Controller in UserApiApplication.

--- users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/example/usersapi/UsersApiApplication.java
@@ -3,13 +3,20 @@ package com.example.usersapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApiApplication.class, args);
+	}
+	@RequestMapping(value = "/")
+	public String home() {
+		return "Eureka Client: Users application";
 	}
 
 }
