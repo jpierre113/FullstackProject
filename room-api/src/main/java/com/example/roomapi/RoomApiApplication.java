9b44efe8c6606d5dcd92327c1003867c5faf9ff8
Janna Pierre <jannapierre@gmail.com>
2019-11-05 18:03:21 -0500

    Created app-dev for room and added room-api to yml. Made updates to room pom.xml

--- room-api/src/main/java/com/example/roomapi/RoomApiApplication.java
+++ room-api/src/main/java/com/example/roomapi/RoomApiApplication.java
@@ -2,12 +2,21 @@ package com.example.roomapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class RoomApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomApiApplication.class, args);
+	}
+	@RequestMapping(value = "/room")
+	public String home() {
+		return "Eureka Client: Room application";
 	}
 
 }
