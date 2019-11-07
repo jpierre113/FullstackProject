d53b98210454a3a608b50c69eac9a0dff84a986a
Janna Pierre <jannapierre@gmail.com>
2019-11-07 13:40:08 -0500

    Building project from scratch. Added dependanciesand created User model.

--- HotelReservation/src/main/java/com/example/HotelReservation/model/User.java
+++ HotelReservation/src/main/java/com/example/HotelReservation/model/User.java
@@ -1,4 +1,42 @@
 package com.example.HotelReservation.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
-public class User {
+@Entity(name = "User")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName(){return firstName;}
+
+    public void setFirstName(String firstName){this.firstName = firstName;}
+
+    public String getLastName(){return lastName;}
+
+    public void setLastName(String lastName){this.lastName = lastName;}
+
+    public String getEmail(){return email;}
+
+    public void setEmail(String email){this.email = email;}
+
+    public String getPassword(){return password;}
+
+    public void setPassword(String password){this.password = password;}
 }
