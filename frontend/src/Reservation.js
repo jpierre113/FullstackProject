1d54575a27a2d0362bf3b35fc2bc4e861998d556
Janna Pierre <jannapierre@gmail.com>
2019-11-06 21:03:50 -0500

    Installed DayPicker and implemented in Reservation.

--- frontend/src/Reservation.js
+++ frontend/src/Reservation.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
 
-class Reservation extends Component {
-  render(){
+const Reservation = (props) => {
+
     return(
       <div className="Reservation">
       <h1>Make a Reservation</h1>
+      <DayPickerInput
+      dayPickerProps={{
+        month: new Date(2019, 10),
+        showWeekNumbers: true,
+        todayButton: 'Today',
+      }}
+    />
       </div>
     );
-  }
+
 }
   export default Reservation;
