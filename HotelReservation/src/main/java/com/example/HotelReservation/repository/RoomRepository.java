8371f9c97c1579aa6132d266bdc2e4a5270aaa1d
Janna Pierre <jannapierre@gmail.com>
2019-11-09 11:00:57 -0500

    Created RoomRepository.

---
+++ HotelReservation/src/main/java/com/example/HotelReservation/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.example.HotelReservation.repository;
+
+import com.example.HotelReservation.model.Room;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
